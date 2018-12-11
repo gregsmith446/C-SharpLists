490fbc6b0f3fbe9789225d98bda882f9c1b0385e
Greg Smith <gregsmith446@gmail.com>
2018-12-11 12:04:14 -0500

    working with basic lists

---
+++ MoshHelloWorld/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoshHelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var numbers = new List<int>() { 1, 2, 3, 4 };
+
+            foreach (var num in numbers)
+            {
+                Console.WriteLine(num);
+            }
+
+            numbers.Add(1);
+            foreach (var num in numbers)
+            {
+                Console.WriteLine(num);
+            }
+
+            numbers.AddRange(new int[3] { 5, 6, 7 });
+            foreach (var num in numbers)
+            {
+                Console.WriteLine(num);
+            }
+        }
+    }
+}

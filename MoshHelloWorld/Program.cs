c7c00f5f7ddbb81c26389f06fe268778d671d875
Greg Smith <gregsmith446@gmail.com>
2018-12-11 22:00:45 -0500

    lists and list methods

--- MoshHelloWorld/Program.cs
+++ MoshHelloWorld/Program.cs
@@ -15,20 +15,49 @@ namespace MoshHelloWorld
 
             foreach (var num in numbers)
             {
-                Console.WriteLine(num);
+                Console.Write(num);
             }
+
+            Console.WriteLine();
 
             numbers.Add(1);
             foreach (var num in numbers)
             {
-                Console.WriteLine(num);
+                Console.Write(num);
             }
 
+            Console.WriteLine();
+
             numbers.AddRange(new int[3] { 5, 6, 7 });
+
+            foreach (var num in numbers)
+            {
+                Console.Write(num);
+            }
+
+            Console.WriteLine();
+
+            Console.WriteLine("Index of the first 1 in list is: " + numbers.IndexOf(1)); ;
+            Console.WriteLine("Index of the second 1 in the list is: " + numbers.LastIndexOf(1)); ;
+
+            Console.WriteLine("Count here is: " + numbers.Count);
+
+            for (var i = 0; i < numbers.Count; i++)
+            {
+                if (numbers[i] == 1)
+                    numbers.Remove(numbers[i]);
+            }
+
             foreach (var num in numbers)
             {
                 Console.WriteLine(num);
             }
+
+            numbers.Clear();
+            Console.WriteLine("Final Count is: " + numbers.Count);
+
+
+
         }
     }
 }
